27f53cb59700ae78ee94860bad834dd12f26171c
Haik <9haik1@gmail.com>
2023-03-08 20:53:14 -0300

    Ajuste ambiente inicial da api

--- src/controllers/Index.Controller.js
+++ src/controllers/Index.Controller.js
@@ -2,12 +2,8 @@ import Usuario from '../models/Usuario';
 
 class IndexController {
   async index(req, res) {
-    const usuario = await Usuario.create({
-      login: 'x',
-      senha: 'x',
-      email: 'x',
-    });
-    res.json(usuario);
+    console.log(res.status(200));
+    res.json(req.body.msg);
   }
 }
 
